0e035ebea26251a6bdb18b70640c4bb9ecc89110
Gabriel Alfaro <gabriel.alfaro.cruz@gmail.com>
2023-09-01 15:47:31 -0600

    Actualizado a https

--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -31,12 +31,12 @@ const ClimaProvider = ({children}) => {
       const appId = import.meta.env.VITE_API_KEY;
 
 
-      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`
+      const url = `https://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`
       const {data} = await axios(url)
       const {lat,lon} = data[0]; //data[0] porque lo trae como un array
 
-      const urlClima = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
-      // console.log(urlClima)
+      const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+      console.log(urlClima)
       const {data:clima} = await axios(urlClima)
       // console.log(clima)
       setResultado(clima)
